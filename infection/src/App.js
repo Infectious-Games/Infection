aebeda7209dc042122e1b17f7f60a769bb6efc16
Mark Stark <markstarkart@gmail.com>
2018-08-14 12:02:54 -0500

    infection logo and flavicon working will move into welcome page latee

--- infection/src/App.js
+++ infection/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-import logo from './logo.svg';
+import infectionLogo from './Infection.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+          <img src={infectionLogo} className="App-infection-logo" alt="logo" />
           <h1 className="App-title">INFECTION</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
+        </p>
       </div>
     );
   }
