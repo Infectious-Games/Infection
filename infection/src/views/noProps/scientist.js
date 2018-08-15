d3be2fc330828e15353358ba5f57c5aba9c35bbe
Mark Stark <markstarkart@gmail.com>
2018-08-15 18:47:24 -0500

    added views for roles , scientist and infiltrator with simple logic

---
+++ infection/src/views/noProps/scientist.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Scientist = () => 
+  <div>I'm a Scientist</div>
+
+
+  export default Scientist;
---
+++ infection/src/views/withProps/infiltrator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Infiltrator = (props) => 
+  <div>I'm a Infiltrator, I have props</div>
+
+
+  export default Infiltrator;
