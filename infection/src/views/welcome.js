6bbc95f501f2dcdf10d0878512803fc6d3b8c148
Mark Stark <markstarkart@gmail.com>
2018-08-14 23:41:22 -0500

    login component with state rendering to welcome view. some issues, handleSubmit not logig properly and e.preventDefault not preventing

--- infection/src/views/welcome.js
+++ infection/src/views/welcome.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import infectionLogo from '../Infection.jpg';
+import Login from '../components/login';
 
-const Welcome = (props) => {
+const Welcome = () => {
  return <div>
     <header className="welcome-header">
       <img src={infectionLogo} className="App-infection-logo" alt="logo" />
       <h1 className="welcome-title">Welcome to the team!</h1>
         <p>Our mission is to stop infectious outbreaks threatening the United States</p>
     </header>
-    <div>login Component</div>
-    {/* <login></login> */}
+    <Login></Login>
   </div>
 }
 
