05b6f41e4fb80b9698beaf54fe8c588c001d576e
aturek14@gmail.com <aturek14@gmail.com>
2018-08-16 14:07:38 -0500

    passing socket connection to all necessary components

--- infection/src/components/hasState/game.js
+++ infection/src/components/hasState/game.js
@@ -1,20 +1,31 @@
 import React, { Component} from 'react';
-import io from 'socket.io-client';
 
 import Roles from '../../views/withProps/roles';
+import socket from '../../socket';
+import { Button } from 'react-bootstrap';
 
-const socket = io();
 
 class Game extends Component {
   constructor(props) {
     super(props);
+
+    this.checkGameStatus = this.checkGameStatus.bind(this);
+
     this.state = {
       username: this.props.username,
       infiltrator: this.props.infiltrator,
     }
   }
+  componentDidMount() {
+    this.checkGameStatus();
+  }
 
-  //set up socket listeners. set received data to props
+  checkGameStatus(){
+    socket.on('game start', (players)=>{
+      console.log(players, 'players');
+    })
+  }
+
   render() {
     return <div>Gimme some props
       <Roles infiltrator={ this.state.infiltrator }></Roles>
