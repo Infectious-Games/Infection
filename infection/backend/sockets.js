dc45dd231ef744ca17d62231fce60b059cca3ee1
aturek14@gmail.com <aturek14@gmail.com>
2018-08-16 00:22:53 -0500

    pulling all sockets connected to a room

--- infection/backend/sockets.js
+++ infection/backend/sockets.js
@@ -1,38 +1,58 @@
 const sockets = require('socket.io');
 
+
 module.exports = (server) => {
     const io = sockets(server);
-    //socket.on('eventName', callback)
-        //e.g. (data) => console.log(data);
+    const storeUsers = (game) => {
+        const namespace = io.of('/');
+        namespace.in(game).clients((error, clients) => {
+            if (error) {
+                throw error;
+            } else {
+                let players = clients.map(client => namespace.connected[client]);
+                console.log(players, 'players');
+        // FIXME: player sockets exist here. conditional below causes max call stack error
+                // if (players.length === 4) {
+                //     // send this array to store as players for the game
+                //     io.in(game).emit('start game', { players });
+                // } else {
+                //     io.in(game).emit('room not full');
+                // }
+            }
+        });
+    };
     io.on('connection', (socket) => {
 
       socket.on('join game', (playerProps) => {
         const game = playerProps.game;
         const username = playerProps.username;
+        socket.game = game;
+        socket.username = username;
         console.log(`${username} has joined ${game}`, playerProps);
-
     //SERVER CONNECTS PLAYER TO GAME---------------------------------------------------------------------------
         socket.join(game);
         //TODO: insert reducer function to handle storage of all players associated with game
-        //TODO: insert conditional  that checks if this is the last user needed to start the game, if so call emitter below
-        //STARTS GAME WITH ROLE ASSIGNMENTS---------------------------------------------------------------------------
-        io.in(game).emit('start game'); /* TODO: second arg will be players in store for this game, with role assignments */
+        //STARTS GAME WITH ROLE ASSIGNMENTS---------------------------------------------------------------------------        
+        let gameStartStatus = storeUsers(game);
+        console.log(gameStartStatus);
     });
 
     //NEW ROUND-------------------------------------------------------------------------------------------------
+    //instead of a new round event, put the leader chosen emitter in a setTimeout here and below which implicitely starts new round
     socket.on('start round', () => {
         //TODO: get leader from store
         io.in(game).emit('leader chosen'); /* TODO: second arg will be leader's username */
     });
     //LEADER CHOSE TEAM----------------------------------------------------------------------------------------
     socket.on('deploy team', (team) => {
-        io.in(game).emit('team chosen'); /* TODO: second arg will be all team members deployed */ 
+        io.in(game).emit('team chosen', team);
     })
     //CURE OR SABOTAGE CHOSEN-----------------------------------------------------------------------------------
     socket.on('chose cure or sabotage', (choice) => {
         //TODO: update state of game according to the choice submitted
         let results; /* TODO: assign results to the current mission results and game state */
         io.in(game).emit('results', results);
+        //setTimeout on leader chosen emitter to start next round IF results are not final game results
     })
     //DISCONNECT SOCKET-----------------------------------------------------------------------------------------
     // socket.on('disconnect', () => {})
