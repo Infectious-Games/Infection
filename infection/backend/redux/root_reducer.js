da5b5ef84e31158399c9bddeef0fc28e762010a7
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-15 22:10:31 -0500

    Added new modules

--- infection/backend/redux/root_reducer.js
+++ infection/backend/redux/root_reducer.js
@@ -2,14 +2,16 @@ const { combineReducers } = require('redux');
 
 const users = require('./users/reducer_users');
 const rounds = require('./rounds/reducer_rounds');
-const teamVotes = require('./teamVotes/reducer_teamVotes');
+const game = require('./game/reducer_game');
+const cureOrSabotage = require('./cureOrSabotage/reducer_cureOrSabotage');
 const leader = require('./leader/reducer_leader');
 const winner = require('./winner/reducer_winner');
 
 module.exports = combineReducers({
   users,
   rounds,
-  teamVotes,
+  game,
+  cureOrSabotage,
   leader,
   winner
 });
