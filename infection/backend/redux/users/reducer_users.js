5039d02b58444ed305fcc39f29acc8aae1153b2c
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-17 16:53:40 -0500

    Concatenates new users to users array.

--- infection/backend/redux/users/reducer_users.js
+++ infection/backend/redux/users/reducer_users.js
@@ -1,4 +1,4 @@
-const { UPDATE_USERS, } = require('./actions_users.js')
+const { UPDATE_USERS, SET_USERNAME, ADD_NEW_USER } = require('./actions_users.js')
   
 const initialState = require('./initialState_users.js');
 
@@ -10,29 +10,37 @@ let newState = Object.assign({}, state);
 switch (action.type) {
   // When New Users Join the Room  
   case UPDATE_USERS:
-    if (action.player.id) {
-      //TODO: Connect to specific user on db/socket
-      let currentPlayer = xxxxxx;
-      if (!currentPlayer) {
-        //TODO: See above
-        xxxxx = {
-          leader: false,
-          username: '',
-          infiltrator: false
-        }
-      } 
-    }
-    return newState;
+  return state;
+    // if (action.player.id) {
+    //   //TODO: Connect to specific user on db/socket
+    //   let currentPlayer = xxxxxx;
+    //   if (!currentPlayer) {
+    //     //TODO: See above
+    //     xxxxx = {
+    //       leader: false,
+    //       username: '',
+    //       infiltrator: false
+    //     }
+    //   } 
+    // }
+    // return newState;
+  case ADD_NEW_USER: 
+    //TODO: get socketID to track unique
+    return state.concat([{
+      username: action.username, 
+      room: action.room,
+      socketID: action.socketID,
+      role: null,
+      securityOfficer: false,
+    }]);
   
   case SET_USERNAME:
-    if (newState[action.roomId][action.id]) {
-      newState[action.roomId][action.id].username = action.username;
-    }
+    //TODO:
     return newState;
   // Return current state if incorrect action passed
   default:
     return state;
-}
+  }
 }
 
 module.exports = users;
