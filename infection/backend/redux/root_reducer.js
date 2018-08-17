c3711f611ed70ae61ca713b289294b64fa38d281
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-16 19:31:04 -0500

    Added test for store test on socket

--- infection/backend/redux/root_reducer.js
+++ infection/backend/redux/root_reducer.js
@@ -6,14 +6,16 @@ const game = require('./game/reducer_game');
 const cureOrSabotage = require('./cureOrSabotage/reducer_cureOrSabotage');
 const leader = require('./leader/reducer_leader');
 const winner = require('./winner/reducer_winner');
+const test = require('./test/reducer_test');
 
 module.exports = combineReducers({
-  users,
-  rounds,
-  game,
-  cureOrSabotage,
-  leader,
-  winner
+  // users,
+  // rounds,
+  // game,
+  // cureOrSabotage,
+  // leader,
+  // winner,
+  test
 });
 
 module.exports = { rootReducer };
