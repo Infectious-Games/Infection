9c168291e4464d8400502903931cff696f18eb3d
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-16 19:27:57 -0500

    Test Directory for testing store in socket

---
+++ infection/backend/redux/test/reducer_test.js
@@ -0,0 +1,22 @@
+const { HEY_WORLD, GOODBYE_WORLD } = require('./actions_test');
+const initialState = require('./initialState_test')
+
+const howdy = (state = initialState, action) => {
+  switch (action.type) {
+    case HEY_WORLD:
+      return {
+        hello: 'world',
+        completed: true
+      }
+      break;
+      case GOODBYE_WORLD:
+      return {
+        hello: 'goodbye',
+        completed: true
+      }
+    default:
+      return state;
+  }
+}
+
+module.exports = howdy;
