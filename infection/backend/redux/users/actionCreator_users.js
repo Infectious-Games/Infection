db603e8611bb6f113dda6a5ca3fbe264891c3ddd
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-16 19:33:04 -0500

    Constants defined for user actions

---
+++ infection/backend/redux/users/actionCreator_users.js
@@ -0,0 +1,36 @@
+
+const updateUser = (id, room, username) => ({
+  
+  type: UPDATE_USER,
+  // User input from the client 
+  username,
+  // From the socket 
+  room, 
+  // Socket ID 
+  id, 
+});
+
+const newUser = (username, game, role, leader, securityOfficer) => {
+  return {
+    type: ADD_NEW_USER,
+    username,
+    game,
+    role = null,
+    leader = false,
+    securityOfficer = false
+  }  
+}
+
+const setUsername = (id, room, username) => ( {
+  type: SET_USERNAME,
+  id,
+  username,
+  room
+})
+
+module.exports = {
+  updateUser, 
+  setUsername, 
+  newUser
+}
+
