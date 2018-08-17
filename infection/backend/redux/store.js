4c6f220eb19f3991fc11dbc16b23c0c474cd468b
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-16 20:45:59 -0500

    Properly returning store object

--- infection/backend/redux/store.js
+++ infection/backend/redux/store.js
@@ -3,3 +3,7 @@ const { createStore } = require('redux');
 const reducer = require('./root_reducer');
 
 module.exports = createStore(reducer);
+
+// console.log(module.exports);
+console.log(module.exports.getState());
+console.log(module.exports.getState().test);
