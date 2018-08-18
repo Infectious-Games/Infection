b9c2b846a3a99e6184cac03bb6403c5f2a993e97
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-17 21:03:12 -0500

    Assigning random roles

--- infection/backend/redux/users/reducer_users.js
+++ infection/backend/redux/users/reducer_users.js
@@ -1,4 +1,4 @@
-const { UPDATE_USERS, SET_USERNAME, ADD_NEW_USER } = require('./actions_users.js')
+const { UPDATE_USERS, SET_USERNAME, ADD_NEW_USER, ASSIGN_ROLES } = require('./actions_users.js')
   
 const initialState = require('./initialState_users.js');
 
@@ -30,17 +30,26 @@ switch (action.type) {
       username: action.username, 
       room: action.room,
       socketID: action.socketID,
-      role: null,
+      infiltrator: false,
       securityOfficer: false,
     }]);
   
   case SET_USERNAME:
-    //TODO:
+    //TODO: Probably will not be using this now.
     return newState;
-  // Return current state if incorrect action passed
+  
+  case ASSIGN_ROLES:
+    // TODO: When we go to 5 players, see below + change assignment
+    // let infiltratorCount = ~~(state.length * .44);
+    let ind = ~~(Math.random() * state.length);
+    state[ind].infiltrator = true;
+      
   default:
     return state;
   }
 }
 
 module.exports = users;
+
+// let users2 = Array(3).fill(users, 0);
+// let merged = users2.concat.apply([], users2);
