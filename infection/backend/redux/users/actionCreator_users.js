71297495919d5dd9debdefc1ba4207dcb7aad48b
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-18 21:18:42 -0500

    Linter working. Error cleanup.

--- infection/backend/redux/users/actionCreator_users.js
+++ infection/backend/redux/users/actionCreator_users.js
@@ -1,7 +1,6 @@
 const { UPDATE_USER, ADD_NEW_USER, SET_USERNAME, ASSIGN_ROLES } = require('./actions_users');
 
 const updateUser = (id, room, username) => ({
-  
   type: UPDATE_USER,
   // User input from the client 
   username,
@@ -11,33 +10,31 @@ const updateUser = (id, room, username) => ({
   id, 
 });
 
-const newUser = (username, game, id, role, securityOfficer) => {
-  return {
-    type: ADD_NEW_USER,
-    username,
-    game,
-    id,
-    role,
-    securityOfficer
-  }  
-}
+const newUser = (username, room, socketID, infiltrator, securityOfficer) => ({
+  type: ADD_NEW_USER,
+  username,
+  room,
+  socketID,
+  infiltrator,
+  securityOfficer
+});
 
 const setUsername = (id, room, username) => ({
   type: SET_USERNAME,
   id,
   username,
   room
-})
+});
 
 const assignRoles = (infiltrator) => ({
   type: ASSIGN_ROLES,
   infiltrator
-})
+});
 
 module.exports = {
   updateUser, 
   setUsername, 
   newUser,
   assignRoles
-}
+};
 
