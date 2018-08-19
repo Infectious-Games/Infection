ac4866e8fb6c630cca85a09f1744af7eb9b8d3c5
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-18 21:17:57 -0500

    Removed sample calls and imports from store

--- infection/backend/redux/store.js
+++ infection/backend/redux/store.js
@@ -1,48 +1,6 @@
 const { createStore } = require('redux');
-
-//TODO: THESE WILL NOT LIVE HERE IN PRODUCTION - - - - >
-const { ADD_NEW_USER } = require('./users/actions_users'); 
-const { sayGoodbye, sayHello } = require('./test/actionCreator_test');
-const { newUser } = require('./users/actionCreator_users');
-const { incrementRound, restartRounds } = require('./rounds/actionCreator_rounds');
-const { voteCure, voteSabotage } = require('./cureOrSabotage/actionCreator_cureOrSabotage');
-const { VOTE_CURE, VOTE_SABOTAGE } = require('./cureOrSabotage/actions_cureOrSabotage.js');
-
 const reducer = require('./root_reducer');
 
 let store = createStore(reducer);
 
 module.exports = store;
-
-
-//TODO: THESE WILL NOT LIVE HERE IN PRODUCTION - - - - >
-
-// console.log(store);
-// store.dispatch(sayGoodbye());
-// store.dispatch(sayHello());
-// store.dispatch(incrementRound()); // goes to 1
-// store.dispatch(incrementRound()); // goes to 2
-// store.dispatch(incrementRound()); // goes to 3
-// store.dispatch(restartRounds()); // goes to 0
-// store.dispatch(incrementRound()); // goes to 1
-// store.dispatch(incrementRound()); // goes to 2
-
-// console.log(module.exports.getState().test);
-// store.dispatch({type: ADD_NEW_USER, username:'matthew', room:'demo', socketID:23434, infiltrator: false, securityOfficer:false})
-// store.dispatch({type: ADD_NEW_USER, username:'athena', room:'demo', socketID:23435, infiltrator: false, securityOfficer:false})
-// store.dispatch({type: ADD_NEW_USER, username:'mark', room:'demo', socketID:23437, infiltrator: false, securityOfficer:false})
-// store.dispatch({type: ADD_NEW_USER, username:'paul', room:'demo', socketID:23438, infiltrator: false, securityOfficer:false})
-// console.log(store.getState().users)
-// store.dispatch(newUser('Mark','demo',345,false,false))
-// store.dispatch(newUser('Matt','demo',346,false,false))
-// store.dispatch(newUser('Athena','demo',347,false,true))
-// store.dispatch(newUser('Paul','demo',348,false,false))
-
-
-
-
-// console.log(store.getState().cureOrSabotage.deployedVoteCount);
-console.log(store.getState(), 'STORE in STORE - initial');
-
-
-// console.log((store.getState().users.length === 4) ? store.getState().users : 'waiting for users')
