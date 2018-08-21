122a2885cc88106ac0e2048f9ff919636f91b6fa
aturek14@gmail.com <aturek14@gmail.com>
2018-08-20 19:31:55 -0500

    changed db credentials to process.env variables

--- infection/backend/database.js
+++ infection/backend/database.js
@@ -1,8 +1,4 @@
 const Sequelize = require('sequelize');
-//const AWS = require('../config');
-
-const dotenv = require('dotenv');
-dotenv.load();
 
 const db = new Sequelize(`mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URI}:3306/${process.env.DATABASE_NAME}`, {})
 
