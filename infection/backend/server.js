dab1e2701080b46c1635823edca70e7ac9ade665
aturek14@gmail.com <aturek14@gmail.com>
2018-08-22 14:04:55 -0500

    move http endpoints to routes.js to separate out concerns

--- infection/backend/server.js
+++ infection/backend/server.js
@@ -19,36 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-// find or add a user to the db
-app.post('/user', (req, res) => {
-  const { body } = req;
-  db.updateUser(body, (data) => {
-    // response is true if user has been added to db, or false if user already exists
-    res.json(data);
-  });
-});
-
-//get join code for new game
-app.post('/start', (req, res) => {
-  //take player count from body and use it to create game instance
-  const { body } = req;
-  const playerCount = body.playerCount;
-  //send join code (unique game id) back to client
-  db.createGameAndGetJoinCode(playerCount, (joinCode) => {
-    res.json(joinCode);
-  });
-});
-
-
-// update user's stats in the db
-app.post('/userStats', (req, res) => {
-  const { body } = req;
-  console.log(body, 'body in server');
-  db.updateUserStats(body, (data) => {
-    res.json(data);
-  });
-});
-
 const server = app.listen(port, (err) => {
     if (err) {
       console.log(err);
