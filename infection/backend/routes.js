98593665be6d59c9658f9615f1450235ed83bbef
aturek14@gmail.com <aturek14@gmail.com>
2018-08-22 14:04:08 -0500

    add http request routes to routes.js to separate concerns

--- infection/backend/routes.js
+++ infection/backend/routes.js
@@ -1,6 +1,30 @@
-//add any static routes covered by http requests
-    //login, logout, createGame?
-    module.exports = (app) => {
-    
-    }
-    
+const db = require('./database');
+
+module.exports = (app) => {        
+// find or add a user to the db
+	app.post('/user', (req, res) => {
+		const { body } = req;
+		db.updateUser(body, (data) => {
+		// response is true if user has been added to db, or false if user already exists
+		res.json(data);
+		});
+	});
+//get join code for new game
+	app.post('/start', (req, res) => {
+		//take player count from body and use it to create game instance
+		const { body } = req;
+		const playerCount = body.playerCount;
+		//send join code (unique game id) back to client
+		db.createGameAndGetJoinCode(playerCount, (joinCode) => {
+		res.json(joinCode);
+		});
+	});
+// update user's stats in the db
+	app.post('/userStats', (req, res) => {
+		const { body } = req;
+		console.log(body, 'body in server');
+		db.updateUserStats(body, (data) => {
+		res.json(data);
+		});
+	});
+}
