db73997878315d6098216a594803f4e7a096e5ec
aturek14@gmail.com <aturek14@gmail.com>
2018-08-23 09:50:57 -0500

    remove find player count function. should be done with redux

--- infection/backend/database.js
+++ infection/backend/database.js
@@ -74,28 +74,6 @@ const createGameAndGetJoinCode = (count, cb) => {
   })
 }
 
-/* Sequelize comes with built in support for promises
- * making it easy to chain asynchronous operations together */
-// User.sync()
-//   .then(function() {
-//     // Now instantiate an object and save it:
-//     return User.create({username: 'Jean Valjean'});
-//   })
-//   .then(function() {
-//     // Retrieve objects from the database:
-//     return User.findAll({ where: {username: 'Jean Valjean'} });
-//   })
-//   .then(function(users) {
-//     users.forEach(function(user) {
-//       console.log(user.username + ' exists');
-//     });
-//     db.close();
-//   })
-//   .catch(function(err) {
-//     // Handle any error in the chain
-//     console.error(err);
-//     db.close();
-//   });
 const clearanceLevels = (wins => {
   if (wins < 10) {
     return 'Unclassified';
