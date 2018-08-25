ad61d8d7ee0adab281aad30dcec78d4fff03a9fe
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-25 11:40:45 -0500

    Remove redundant files

--- infection/backend/redux/user/actionCreator_user.js
+++
@@ -1,9 +0,0 @@
-const newUser = (username, game, role, leader, securityOfficer) => {
-  return {
-    username,
-    game,
-    role = null,
-    leader = false,
-    securityOfficer = false
-  }  
-}
