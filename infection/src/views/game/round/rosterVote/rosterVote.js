55227d13bf9f4b2a12819ea5518287503ec9a96e
Mark Stark <markstarkart@gmail.com>
2018-08-25 15:04:10 -0500

    created waiting for roster vote view

--- infection/src/views/game/round/rosterVote/rosterVote.js
+++ infection/src/views/game/round/rosterVote/rosterVote.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
-import Waiting from '../../waiting/waiting';
+import WaitingForRosterVote from '../../waiting/waitingForRosterVote';
 import Vote from './vote';
 
 const RosterVote = ({
@@ -15,16 +15,7 @@ const RosterVote = ({
   votedOnRoster
     ? allUsersVotedOnRoster
       ? <div>List of User Votes {usersVoteRecord[0].name} {usersVoteRecord[0].vote}</div>
-      : <Grid>
-        <Row>
-          <Col md={8}>
-            <h3>YOUR VOTE HAS BEEN RECORDED!</h3>
-            <h4>Please wait for the rest of your Team to Vote.</h4>
-            <br></br>
-            <Waiting></Waiting>
-          </Col>
-        </Row>
-      </Grid>
+      : <WaitingForRosterVote></WaitingForRosterVote>
     : <Vote
       handleRosterVote={handleRosterVote}
       leader={leader}
--- infection/src/views/game/round/rosterVote/vote.js
+++ infection/src/views/game/round/rosterVote/vote.js
@@ -1,73 +1,56 @@
 import React from 'react';
 import { Grid, Row, Col, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
-import Waiting from '../../waiting/waiting';
 
-const Vote = ({ 
-  allUsersVotedOnRoster,
+const Vote = ({
   handleRosterVote,
   leader,
   missionRoster,
-  usersVoteRecord,
-  votedOnRoster, 
 }) =>
-  votedOnRoster
-    ? allUsersVotedOnRoster
-      ? <div>List of User Votes {usersVoteRecord[0].name} {usersVoteRecord[0].vote}</div>
-      : <Grid>
-        <Row>
-          <Col md={8}>
-            <h3>YOUR VOTE HAS BEEN RECORDED!</h3>
-            <h4>Please wait for the rest of your Team to Vote.</h4>
-            <br></br>
-            <Waiting></Waiting>
-          </Col>
-        </Row>
-      </Grid>
-    : <Grid>
-      <Row>
-        <Col md={8}>
-          <h2>
-            {leader} selects the following for mission
+  <Grid>
+    <Row>
+      <Col md={8}>
+        <h2>
+          {leader} selects the following for mission
         </h2>
-          <br></br>
-          <ListGroup>
-            {
-              missionRoster.map(member =>
-                <ListGroupItem
-                  key={member}
-                >
-                  {member}
-                </ListGroupItem>
-              )
-            }
-          </ListGroup>
-          <br></br>
-          <h2>Do you approve?</h2>
-          <br></br>
-          <Row>
-            <Button
-              onClick={() => handleRosterVote('YES')}
-              bsSize="large"
-              bsStyle="success"
-            >YES</Button>
-          </Row>
-          <br></br>
-          <Row>
-            ---OR---
+        <br></br>
+        <ListGroup>
+          {
+            missionRoster.map(member =>
+              <ListGroupItem
+                key={member}
+              >
+                {member}
+              </ListGroupItem>
+            )
+          }
+        </ListGroup>
+        <br></br>
+        <h2>Do you approve?</h2>
+        <br></br>
+        <Row>
+          <Button
+            onClick={() => handleRosterVote('YES')}
+            bsSize="large"
+            bsStyle="success"
+          >YES</Button>
+        </Row>
+        <br></br>
+        <Row>
+          ---OR---
             </Row>
-          <br></br>
-          <Row>
-            <Button
-              onClick={() => handleRosterVote('NO')}
-              bsSize="large"
-              bsStyle="danger"
-            >NO</Button>
-          </Row>
-        </Col>
-      </Row>
-    </Grid>
-  
-  
+        <br></br>
+        <Row>
+          <Button
+            onClick={() => handleRosterVote('NO')}
+            bsSize="large"
+            bsStyle="danger"
+          >NO</Button>
+        </Row>
+      </Col>
+    </Row>
+  </Grid>
+
+
 
 export default Vote;
