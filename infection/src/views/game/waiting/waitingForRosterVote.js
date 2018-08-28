ccf307473d4e7e0ba3a500b6c84d24d9dd688eec
Mark Stark <markstarkart@gmail.com>
2018-08-28 14:36:34 -0500

    refactored BS better on sm screens

--- infection/src/views/game/waiting/waitingForRosterVote.js
+++ infection/src/views/game/waiting/waitingForRosterVote.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import Waiting from './waiting';
 
 const WaitingForRosterVote = () => 
-  <Grid>
-    <Row>
-      <Col md={8}>
-        <h3>Your Vote Has Been Recorded!</h3>
-        <h4>Please Wait For The Rest Of Your Team To Vote.</h4>
-        <br></br>
-        <Waiting></Waiting>
-      </Col>
-    </Row>
-  </Grid>
+  <Row>
+    <h3>Your Vote Has Been Recorded!</h3>
+    <h4>Please Wait For The Rest Of Your Team To Vote.</h4>
+    <Col md={2}></Col>
+    <Col md={8}>
+      <br></br>
+      <Waiting></Waiting>
+    </Col>
+    <Col md={2}></Col>
+  </Row>
+
 
 export default WaitingForRosterVote;
