a0a825da17961897c5970c74e8db78df4c4b7d53
Mark Stark <markstarkart@gmail.com>
2018-08-28 11:54:31 -0500

    Refactored scientist view from image to html

--- infection/src/views/game/roles/scientist.js
+++ infection/src/views/game/roles/scientist.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Col, Row } from 'react-bootstrap';
 
-import scientist from '../../../images/Scientist-Card.png';
+import scientist from '../../../images/Scientist.png';
 
 const Scientist = () => 
-  <Image src={scientist} responsive></Image>
+<Row className='scientist'>
+  <br></br>
+  <h4>YOU'VE BEEN ADDED TO THE TASK FORCE!</h4>
+  <Col md={4} ></Col>
+  <Col md={4} xs={12}>
+    <Image src={scientist} responsive></Image>
+    <h1 className='scientist-title'>SCIENTIST</h1> 
+    <h3>YOUR OBJECTIVES:</h3>
+    <ul>
+      <li>Administer Cure</li>
+      <li>Contain Outbreaks</li>
+      <li>Uncover Infiltrators</li>
+    </ul>
+  </Col>
+  <Col md={4}></Col>
 
+  </Row>
   export default Scientist;
   
