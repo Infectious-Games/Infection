2343c17d29a1ef9cb98e5f5de07ecaaf501c0841
Mark Stark <markstarkart@gmail.com>
2018-08-28 20:24:51 -0500

    refactored BS for leader, and selected list, looks decent on ipone6

--- infection/src/views/game/round/rosterVote/roster/leader.js
+++ infection/src/views/game/round/rosterVote/roster/leader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 
 import SelectRoster from './selectRoster';
@@ -10,19 +10,18 @@ const Leader = ({
   handleSubmitRoster,
   rosterLength, 
 }) => 
-  <Grid>
     <Row>
-      <Col md={8}>
-        <h1>Congratulations You Are The Leader</h1>
-        <h2>Set Your Mission Roster</h2>
-        <h4>Select {rosterLength} Scientists</h4>
+      <h3>Congratulations Leader</h3>
+      <h5>Select {rosterLength} Scientists For Mission Roster</h5>
+      <Col md={4}></Col>
+      <Col md={4}>
         <SelectRoster 
           team={team}
           handleSelectRosterEntryClick={handleSelectRosterEntryClick}
           handleSubmitRoster={handleSubmitRoster}
         ></SelectRoster>
       </Col>
+      <Col md={4}></Col>
     </Row>
-  </Grid>
 
 export default Leader;
--- infection/src/views/game/round/rosterVote/roster/selectRoster.js
+++ infection/src/views/game/round/rosterVote/roster/selectRoster.js
@@ -1,36 +1,30 @@
 import React from 'react';
-import { Grid, Col, Row, Button, ListGroup } from 'react-bootstrap';
+import { Row, Button, ListGroup } from 'react-bootstrap';
 
 import SelectRosterEntry from './selectRosterEntry';
 
 const SelectRoster = ({ team, handleSelectRosterEntryClick, handleSubmitRoster }) => 
-  <Grid>
+
     <Row>
-      <Col md={8}>
-        <Row>
-          <ListGroup>
-            {
-              team.map((member) => 
-              <SelectRosterEntry
-              active
-              key={member} //change in future
-              member={member}
-              handleSelectRosterEntryClick={handleSelectRosterEntryClick}
-              ></SelectRosterEntry>)
-            }
-          </ListGroup>
-        </Row>
-        <Row>
-          <Button 
-            bsStyle="danger" 
-            bsSize="large" 
-            onClick={handleSubmitRoster}
-          >
-            SUBMIT ROSTER
-          </Button>
-        </Row>
-      </Col>
+      <ListGroup>
+        {
+          team.map((member) => 
+          <SelectRosterEntry
+          active
+          key={member} //change in future
+          member={member}
+          handleSelectRosterEntryClick={handleSelectRosterEntryClick}
+          ></SelectRosterEntry>)
+        }
+      </ListGroup>
+      <Button 
+        bsStyle="danger" 
+        bsSize="large" 
+        onClick={handleSubmitRoster}
+      >
+        SUBMIT ROSTER
+      </Button>
     </Row>
-  </Grid>
+  
 
 export default SelectRoster;
