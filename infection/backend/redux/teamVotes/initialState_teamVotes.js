80ee5f78f46ba0b0017fb671c806bdfe58a9edeb
aturek14@gmail.com <aturek14@gmail.com>
2018-08-30 09:06:48 -0500

    potential for multiple concurrent game states

--- infection/backend/redux/teamVotes/initialState_teamVotes.js
+++ infection/backend/redux/teamVotes/initialState_teamVotes.js
@@ -1,5 +1,52 @@
 module.exports = {
-  voteFail: 0,
-  voteSuccess: 0,
-  totalMissionVotes: 0,
+  A1D4: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  X3ZK: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  B5R9: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  F9K0: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  J4H8: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  V7N5: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  C6FR: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  N9M6: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  E6S5: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  },
+  G6U2: {
+    voteFail: 0,
+    voteSuccess: 0,
+    totalMissionVotes: 0,
+  }
 };
