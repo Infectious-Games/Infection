f8a99ee928df12582d145fd0382597c4e71200b1
aturek14@gmail.com <aturek14@gmail.com>
2018-08-30 16:32:57 -0500

    fix linting error

--- infection/backend/redux/rounds/reducer_rounds.js
+++ infection/backend/redux/rounds/reducer_rounds.js
@@ -1,17 +1,14 @@
-const { 
-  INCREMENT_ROUND, 
-  RESTART_ROUNDS 
-} = require('./actions_rounds');
+const { INCREMENT_ROUND, RESTART_ROUNDS } = require('./actions_rounds');
 const initialState = require('./initialState_rounds');
 
 const rounds = (state = initialState, action) => {
   const newState = Object.assign({}, state);
   switch (action.type) {
     case INCREMENT_ROUND:
-      newState[action.gameID]['round'] = state.round + 1;
+      newState[action.gameID].round = state.round + 1;
       return newState;
     case RESTART_ROUNDS:
-      newState[action.gameID]['round'] = 0;
+      newState[action.gameID].round = 0;
       return newState;
     default:
       return state;
