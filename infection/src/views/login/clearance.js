f43b733cf58821fc261da0de83e42201544c17f4
Mark Stark <markstarkart@gmail.com>
2018-08-30 14:33:25 -0500

    removed Header from clearance, no logo in header of view

--- infection/src/views/login/clearance.js
+++ infection/src/views/login/clearance.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import Header from '../game/shared/header';
 
 const Clearance = ({ clearance }) => (
   <Row className={clearance + ' clearance-lvl'}>
-    <br />
-    <Header />
     <Row>{clearance.toUpperCase()}</Row>
   </Row>
 );
