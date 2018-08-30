f7dded0e7012112c3495f02b00b8baec8e9b9ae5
Mark Stark <markstarkart@gmail.com>
2018-08-29 21:26:41 -0500

    added lint rules, refactored game status bar with images instead of buttons

--- infection/src/App.js
+++ infection/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Grid } from 'react-bootstrap';
+
 import './App.css';
 import Login from './components/login';
 import Game from './components/game';
-import { Grid } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.setInGameStatus = this.setInGameStatus.bind(this);
     this.state = {
-    inGame: false,
-    }
+      inGame: false,
+    };
   }
-//pass a function to setInGameStatus to set state.
+  //pass a function to setInGameStatus to set state.
   setInGameStatus() {
     this.setState({ inGame: !this.state.inGame });
     //this should be handled in the store
@@ -20,17 +21,14 @@ class App extends Component {
   render() {
     return (
       <Grid className="App">
-        {
-          this.state.inGame
-            ? <Game
-              setInGameStatus={this.setInGameStatus.bind(this)}
-              ></Game>
-            : <Login setInGameStatus={this.setInGameStatus.bind(this)}></Login>
-        }
+        {this.state.inGame ? (
+          <Game setInGameStatus={this.setInGameStatus.bind(this)} />
+        ) : (
+          <Login setInGameStatus={this.setInGameStatus.bind(this)} />
+        )}
       </Grid>
-    )
+    );
   }
 }
-
 
 export default App;
--- infection/src/views/game/gameStatus/gameStatus.js
+++ infection/src/views/game/gameStatus/gameStatus.js
@@ -1,32 +1,23 @@
+/*eslint-disable*/
 import React from 'react';
-import { Grid, Col, Row, Button} from 'react-bootstrap';
+import { Row, Image } from 'react-bootstrap';
 
-const GameStatus = ({ missionResults }) =>
-  <Grid className="game-results">
-    <Row>
-      <Col md={6}>
-        <h4>Mission Results</h4>
-      </Col>
-    </Row>
-    <br></br>
-    <Row>
-      <Col md={1}
-        bsStyle='default'
-        style={{width: 50}}
-        ></Col>
-        {missionResults.map((round, i) =>
-          <Col md={1} key={i}>
-            {
-              round === 'success'
-                ? <Button bsStyle="success" bsSize="large" ></Button>
-                : round === 'fail'
-                  ? <Button bsStyle="danger" bsSize="large" ></Button>
-                  : <Button bsStyle="default" bsSize="large" ></Button>
-            }
-          </Col>
-        )}
-    </Row>
-  </Grid>
+import success from '../../../images/beaker-green.png';
+import fail from '../../../images/beaker-red.png';
+import undecided from '../../../images/beaker-empty.png';
 
+const GameStatus = ({ missionResults }) => (
+  <Row>
+    <h4>Mission Results</h4>
+    {missionResults.map((round, i) => {
+      round === 'success'
+        ? (round = success)
+        : round === 'fail'
+          ? (round = fail)
+          : (round = undecided);
+      return <Image src={round} height={50} width={30} key={i} />;
+    })}
+  </Row>
+);
 
 export default GameStatus;
