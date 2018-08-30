458cdbdbf470f15b3e43a6955134ecb9414e4f6d
aturek14@gmail.com <aturek14@gmail.com>
2018-08-30 12:30:47 -0500

    refactor for object as new initial state of users

--- infection/backend/redux/users/initialState_users.js
+++ infection/backend/redux/users/initialState_users.js
@@ -1,12 +1,32 @@
 module.exports = {
-  A1D4: [],
-  X3ZK: [],
-  B5R9: [],
-  F9K0: [],
-  J4H8: [],
-  V7N5: [],
-  C6FR: [],
-  N9M6: [],
-  E6S5: [],
-  G6U2: []
+  A1D4: {
+    users: [],
+  },
+  X3ZK: {
+    users: [],
+  },
+  B5R9: {
+    users: [],
+  },
+  F9K0: {
+    users: [],
+  },
+  J4H8: {
+    users: [],
+  },
+  V7N5: {
+    users: [],
+  },
+  C6FR: {
+    users: [],
+  },
+  N9M6: {
+    users: [],
+  },
+  E6S5: {
+    users: [],
+  },
+  G6U2: {
+    users: [],
+  },
 };
--- infection/backend/redux/users/reducer_users.js
+++ infection/backend/redux/users/reducer_users.js
@@ -2,25 +2,26 @@ const { ADD_NEW_USER, ASSIGN_ROLES } = require('./actions_users.js');
 const initialState = require('./initialState_users.js');
 
 const users = (state = initialState, action) => {
-  const newState = Object.assign({}, state);
+  const newState = Object.assign({}, state); // object containing all game objects, each with an array of users
   switch (action.type) {
     // When new user joins the room, create a new array of users with the new user
     /* eslint-disable */
     case ADD_NEW_USER:
-      return newState[action.gameID].concat([
-        {
+      newState[action.gameID].users.concat({
           username: action.username,
           room: action.room,
           socketID: action.socketID,
           infiltrator: false,
           securityOfficer: false,
         },
-      ]);  
+      );
+      return newState;  
     case ASSIGN_ROLES:
       // Determine appropriate number of infiltrators
-      let infiltratorCount = ~~(state[action.gameID].length * .44);
+      const infiltratorCount = ~~(state[action.gameID].users.length * .44);
+    
       // Shuffle users for assignment
-      let arrayShuffled = (array) => {
+      const arrayShuffled = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
           let j = ~~(Math.random() * (i + 1));
           let temp = array[i];
@@ -29,18 +30,18 @@ const users = (state = initialState, action) => {
         }
         return array;
       };
-      let shuffled = arrayShuffled(state[action.gameID]);
+      const shuffled = arrayShuffled(state[action.gameID].users);
+      
       // Assign infiltrator to appropriate number of infiltrators
-      let assigned = shuffled.map((user, index) => {
-        if (index < infiltratorCount) {
-          user.infiltrator = true;
-          return user;
-        } else {
-          user.infiltrator = false;
-          return user;
+      const updated = shuffled.map((user, index) => {
+        (index < infiltratorCount)
+          ? user.infiltrator = true
+          : user.infiltrator = false;
         }
-      });
-      // break;
+      );
+      newState[action.gameID].users = updated;
+      return newState;
+      
     // By default, return current state
     default:
       return newState;
@@ -48,3 +49,4 @@ const users = (state = initialState, action) => {
 };
 
 module.exports = users;
+
