6a898de62b1c0130b50b6ad030344187ca06cdb9
aturek14@gmail.com <aturek14@gmail.com>
2018-08-30 16:28:55 -0500

    store player count in server for each game room

---
+++ infection/backend/gameRooms.js
@@ -0,0 +1,13 @@
+// add player count as value at game key
+module.exports = {
+  A1D4: undefined,
+  X3ZK: undefined,
+  B5R9: undefined,
+  F9K0: undefined,
+  J4H8: undefined,
+  V7N5: undefined,
+  C6FR: undefined,
+  N9M6: undefined,
+  E6S5: undefined,
+  G6U2: undefined,
+};
