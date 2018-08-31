e1d963c60143eeb45929cb0e627f4efb6fa77964
aturek14@gmail.com <aturek14@gmail.com>
2018-08-30 21:54:07 -0500

    resize to match gif

--- infection/src/views/game/missionResults/fail.js
+++ infection/src/views/game/missionResults/fail.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 
-const Fail = () => 
-  <Row className='mission-fail' >
-      <h1 className='display-1 + mission-fail-h'>
-        MISSION
-      </h1>
-      <h1 className='display-1 + mission-fail-h'>
-        SABOTAGE!
-      </h1> 
+const Fail = () => (
+  <Row className="mission-fail" container-fluid>
+    <br />
+    <br />
+    <br />
+    <h1 className="display-1 + mission-fail-h">MISSION</h1>
+    <h1 className="display-1 + mission-fail-h">SABOTAGE</h1>
+    <br />
+    <br />
+    <br />
   </Row>
-  
+);
 
 export default Fail;
