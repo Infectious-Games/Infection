c5688aa928290a399ebf5569fcb688ada644d3cc
aturek14@gmail.com <aturek14@gmail.com>
2018-08-31 18:53:31 -0500

    initial state of room as an object

--- infection/backend/gameRooms.js
+++ infection/backend/gameRooms.js
@@ -1,13 +1,13 @@
 // add player count as value at game key
 module.exports = {
-  A1D4: undefined,
-  X3ZK: undefined,
-  B5R9: undefined,
-  F9K0: undefined,
-  J4H8: undefined,
-  V7N5: undefined,
-  C6FR: undefined,
-  N9M6: undefined,
-  E6S5: undefined,
-  G6U2: undefined,
+  A1D4: {},
+  X3ZK: {},
+  B5R9: {},
+  F9K0: {},
+  J4H8: {},
+  V7N5: {},
+  C6FR: {},
+  N9M6: {},
+  E6S5: {},
+  G6U2: {},
 };
