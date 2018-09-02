0f2fd4e893de4264d32e86bd5716425661c1c8d1
Mark Stark <markstarkart@gmail.com>
2018-09-01 23:19:25 -0500

    fixed linting errors, refactored col for better mobile view

--- infection/src/views/game/round/rosterVote/usersVoteOnRosterList.js
+++ infection/src/views/game/round/rosterVote/usersVoteOnRosterList.js
@@ -4,19 +4,17 @@ import { Col, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
 const UsersVoteOnRosterList = ({ usersVoteRecord }) => (
   <Row>
     <h4>How the Team Voted</h4>
-    <Col md={4} />
-    <Col md={4}>
+    <Col md={4} xs={3} />
+    <Col md={4} xs={6}>
       <ListGroup>
-        {usersVoteRecord.map(user => {
-          return (
-            <ListGroupItem key={user.name}>
-              {user.name} voted {user.vote}
-            </ListGroupItem>
-          );
-        })}
+        {usersVoteRecord.map(user => (
+          <ListGroupItem key={user.name}>
+            {user.name} voted {user.vote}
+          </ListGroupItem>
+        ))}
       </ListGroup>
     </Col>
-    <Col md={4} />
+    <Col md={4} xs={3} />
   </Row>
 );
 
