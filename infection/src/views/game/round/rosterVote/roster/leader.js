92ded3dd6a5f02d5df712ee2d9a87be13ab7b10e
Mark Stark <markstarkart@gmail.com>
2018-09-01 22:19:49 -0500

    small format change, roster list looks better on mobile

--- infection/src/views/game/round/rosterVote/roster/leader.js
+++ infection/src/views/game/round/rosterVote/roster/leader.js
@@ -13,17 +13,17 @@ const Leader = ({
   <Row>
     <h3>Congratulations Leader</h3>
     <h5>Select {rosterLength} Scientists For The Mission</h5>
-    <Col md={4} />
-    <Col md={4}>
+    <Col md={4} xs={3} />
+    <Col md={4} xs={6}>
       <SelectRoster
         team={team}
         roster={roster}
         handleSelectRosterEntryClick={handleSelectRosterEntryClick}
         handleSubmitRoster={handleSubmitRoster}
       />
-      <h6>Selected: {roster.map(member => `${member} `)}</h6>
+      <h6>Selected: {roster.map(member => `*${member}* `)}</h6>
     </Col>
-    <Col md={4} />
+    <Col md={4} xs={3} />
   </Row>
 );
 
