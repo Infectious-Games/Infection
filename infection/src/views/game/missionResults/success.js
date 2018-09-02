d64494ae7f4a5fc486d178f72e6851c29a4ace47
Mark Stark <markstarkart@gmail.com>
2018-09-01 23:43:17 -0500

    fixed linter errors

--- infection/src/views/game/missionResults/success.js
+++ infection/src/views/game/missionResults/success.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 
-
-const Success = () =>
-  <Row className='mission-success' >
-    <h1 className='display-1 + mission-success-h'>
-      MISSION
-      </h1>
-    <h1 className='display-1 + mission-success-h'>
-      SUCCESS!
-      </h1>
+const Success = () => (
+  <Row className="mission-success">
+    <h1 className="display-1 + mission-success-h">MISSION</h1>
+    <h1 className="display-1 + mission-success-h">SUCCESS!</h1>
   </Row>
-  
+);
+
 export default Success;
