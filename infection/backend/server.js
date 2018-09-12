d03a54edf5fb785de80625eee365a8f5a963599d
Paul Marinaro <paulmarinaro@gmail.com>
2018-09-12 17:04:27 -0500

    test

--- infection/backend/server.js
+++ infection/backend/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-// const http = require('http');
-const bodyParser = require('body-parser');
-const routes = require('./routes.js');
-const sockets = require('./sockets.js');
-
-const app = express();
 const port = process.env.PORT || 3005;
 console.log(`using port ${port}`);
-app.set('port', port);
+// const express = require('express');
+// // const http = require('http');
+// const bodyParser = require('body-parser');
+// const routes = require('./routes.js');
+// const sockets = require('./sockets.js');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = express();
+// app.set('port', port);
 
-routes(app);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(port, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.error('listening on port', port);
-  }
-});
+// routes(app);
 
-sockets(server);
+// const server = app.listen(port, err => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.error('listening on port', port);
+//   }
+// });
+
+// sockets(server);
